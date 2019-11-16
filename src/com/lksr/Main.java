7ed52aae7ed55e1abe27556727666e6cc272073a
Lukas <57829499+lksr05@users.noreply.github.com>
2019-11-16 12:44:31 +0100

    Increase b

--- src/com/lksr/Main.java
+++ src/com/lksr/Main.java
@@ -3,7 +3,7 @@ package com.lksr;
 public class Main {
     public static void main(String[] args) {
 	    int a = 2;
-	    int b = 6;
+	    int b = 7;
 	    int c = a + b;
         String message = String.format("Die Summe von %d und %d ist %d.", a, b, c);
 	    System.out.println(message);
