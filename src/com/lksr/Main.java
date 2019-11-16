09af33ed31c9309d10a746b919aa5db41a5b226a
lksr05 <lukas.koser@gmail.com>
2019-11-16 12:40:26 +0100

    Initial Commit

---
+++ src/com/lksr/Main.java
@@ -0,0 +1,11 @@
+package com.lksr;
+
+public class Main {
+    public static void main(String[] args) {
+	    int a = 2;
+	    int b = 6;
+	    int c = a + b;
+        String message = String.format("Die Summe von %d und %d ist %d.", a, b, c);
+	    System.out.println(message);
+    }
+}
