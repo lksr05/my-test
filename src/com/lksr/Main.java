367991b25ac216f74bd267a1979ed2d735a3eb17
Lukas <lukas.koser@gmail.com>
2019-11-17 14:26:03 +0000

    added sqrt

--- src/com/lksr/Main.java
+++ src/com/lksr/Main.java
@@ -1,11 +1,13 @@
 package com.lksr;
+import java.lang.Math;
 
 public class Main {
     public static void main(String[] args) {
 	    int a = 2;
 	    int b = 7;
-	    int c = a + b;
-        String message = String.format("Die Summe von %d und %d ist %d.", a, b, c);
+        int c = a + b;
+        double d = Math.sqrt(c);
+        String message = String.format("Die Summe von %d und %d ist %d - %s.", a, b, c, d);
 	    System.out.println(message);
     }
 }
